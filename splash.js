c12b45eccf71f28a83e6865aa20e24fa5c04dfca
Adam Towers <adam.towers77@gmail.com>
2019-07-02 17:09:04 -0700

    fix some analytics

--- splash.js
+++ splash.js
@@ -1,37 +1,40 @@
 // Gets a reference to the form element, assuming
 // it contains the ID attribute "signup-form".
-var forms = ["form_web", "form_mobile"];
 
-for (var formNumber in forms) {
-  var form = document.getElementById(forms[formNumber]);
+window.onload = function() {
+  var forms = ["form_web", "form_mobile"];
 
-  // Adds a listener for the "submit" event.
-  form.addEventListener("submit", function(event) {
-    console.log("intercepting form submit");
-    // Prevents the browser from submitting the form
-    // and thus unloading the current page.
-    event.preventDefault();
+  for (var formNumber in forms) {
+    var form = document.getElementById(forms[formNumber]);
 
-    // Creates a timeout to call submitForm after one second.
-    setTimeout(submitForm, 1000);
+    // Adds a listener for the "submit" event.
+    form.addEventListener("submit", function(event) {
+      console.log("intercepting form submit");
+      // Prevents the browser from submitting the form
+      // and thus unloading the current page.
+      event.preventDefault();
 
-    // Monitors whether or not the form has been submitted.
-    // This prevents the form from being submitted twice in cases
-    // where the event callback function fires normally.
-    var formSubmitted = false;
+      // Creates a timeout to call submitForm after one second.
+      setTimeout(submitForm, 1000);
 
-    function submitForm() {
-      if (!formSubmitted) {
-        formSubmitted = true;
-        form.submit();
+      // Monitors whether or not the form has been submitted.
+      // This prevents the form from being submitted twice in cases
+      // where the event callback function fires normally.
+      var formSubmitted = false;
+
+      function submitForm() {
+        if (!formSubmitted) {
+          formSubmitted = true;
+          form.submit();
+        }
       }
-    }
-    // Sends the event to Google Analytics and
-    // resubmits the form once the hit is done.
-    gtag("event", "form", {
-      event_label: "splash-signup",
-      event_category: "submit",
-      event_callback: submitForm
+      // Sends the event to Google Analytics and
+      // resubmits the form once the hit is done.
+      gtag("event", "form", {
+        event_label: "splash-signup",
+        event_category: "submit",
+        event_callback: submitForm
+      });
     });
-  });
-}
+  }
+};
